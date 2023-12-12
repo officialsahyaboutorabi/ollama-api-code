99537b38a8bf12c0996ca452cafb6ccf8fbc82f5
officialsahyaboutorabi <152934862+officialsahyaboutorabi@users.noreply.github.com>
2023-12-12 12:34:28 +1100

    Create ollama.py

---
+++ ollama.py
@@ -0,0 +1,34 @@
+import os
+
+class Ollama:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "model": ("STRING", {"default": "ollama/mistral"}),
+                "api_type": ("STRING", {"default": "litellm"}),
+                "api_base": ("STRING", {"default": "http://0.0.0.0:8000"})
+            }
+        }
+
+    RETURN_TYPES = ("LLM",)
+    FUNCTION = "execute"
+    CATEGORY = "AutoGen/LLM"
+
+    def execute(self, model, api_type, api_base):
+        config_list = [
+            {
+                'model': model,
+                'api_type': api_type,
+                'api_base': api_base,
+
+            }
+        ]
+        return ({"LLM": config_list, "llama-cpp": False},)
+
+NODE_CLASS_MAPPINGS = {
+    "Ollama": Ollama,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Ollama": "Ollama"
+}
